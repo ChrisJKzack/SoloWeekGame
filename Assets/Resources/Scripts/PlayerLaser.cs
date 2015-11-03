1ca7aa111fa622679861df617a7ee91360550897
Christopher <ChrisJKZack@Gmail.com>
2015-11-02 23:35:54 -0500

    Switching Game to correct repository

---
+++ Assets/Resources/Scripts/PlayerLaser.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLaser : MonoBehaviour {
+    float shotSpeed = 2;
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        transform.Translate(new Vector3(0, Time.deltaTime * shotSpeed, 0));
+	}
+}
