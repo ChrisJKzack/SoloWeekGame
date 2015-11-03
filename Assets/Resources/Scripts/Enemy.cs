4514d58a8d1300468b60cda1c40c2668135e8f9f
Christopher <ChrisJKZack@Gmail.com>
2015-11-03 11:48:41 -0500

    Added enemy and enemy laser

---
+++ Assets/Resources/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    float shotTimer = 1;
+    bool leftShot = true;
+
+	void Update ()
+    {
+        if (shotTimer <= 0)
+        {
+            if (leftShot)
+            {
+                Instantiate(Resources.Load("Prefab/Enemy/Laser/1"), transform.position + new Vector3(-.25f, -.25f, 0), Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(Resources.Load("Prefab/Enemy/Laser/1"), transform.position + new Vector3(.25f, -.25f, 0), Quaternion.identity);
+            }
+
+            leftShot = !leftShot;
+            shotTimer = 1;
+        }
+
+        shotTimer -= Time.deltaTime;
+	
+	}
+}
