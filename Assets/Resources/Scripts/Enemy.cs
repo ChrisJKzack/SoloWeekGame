a0edeb8f088c3b820870b8c3bc6bc68ebfd1adbc
Christopher <ChrisJKZack@Gmail.com>
2015-11-21 00:51:25 -0500

    Super Attack working and killing enemys, Proably want to make it kill prjectiles

--- Assets/Resources/Scripts/Enemy.cs
+++ Assets/Resources/Scripts/Enemy.cs
@@ -6,37 +6,48 @@ public class Enemy : MonoBehaviour {
     float shotTimer = 1;
     bool leftShot = true;
     public float health = 20;
+    bool dead = false;
 
-	void Update ()
+
+    void Update ()
     {
-        if (shotTimer <= 0)
+        if (!dead)
         {
-            if (leftShot)
+            if (shotTimer <= 0)
             {
-                Instantiate(Resources.Load("Prefab/Enemy/Laser/1"), transform.position + new Vector3(-.25f, -.3f, 0), Quaternion.identity);
-            }
-            else
-            {
-                Instantiate(Resources.Load("Prefab/Enemy/Laser/1"), transform.position + new Vector3(.25f, -.3f, 0), Quaternion.identity);
+                if (leftShot)
+                {
+                    Instantiate(Resources.Load("Prefab/Enemy/Laser/1"), transform.position + new Vector3(-.25f, -.3f, 0), Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(Resources.Load("Prefab/Enemy/Laser/1"), transform.position + new Vector3(.25f, -.3f, 0), Quaternion.identity);
+                }
+
+                leftShot = !leftShot;
+                shotTimer = 1;
             }
 
-            leftShot = !leftShot;
-            shotTimer = 1;
+            shotTimer -= Time.deltaTime;
+
+
+
+            if (health <= 0)
+            {
+                dead = true;
+                GetComponent<Animator>().SetTrigger("Explosion");
+                GetComponent<PolygonCollider2D>().enabled = false;
+            }
+
+        }
+        else
+        {
+            if (gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Dead"))
+            {
+                Destroy(gameObject);
+            }
         }
 
-        shotTimer -= Time.deltaTime;
-
-        if (health <= 0)
-        {
-            Death();
-        }
-	
-	}
-
-    void Death()
-    {
-        Destroy(gameObject);
     }
-
 
 }
